d4ccd70075946177c2b1ff922bbbbfa08e743e93
Alexander Guzman <nocturnal.classical@gmail.com>
2018-05-14 16:10:48 -0500

    agregados metodos para editar un articulo y el index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     # render plain: params[:article].inspect
     # @article = Article.new(article_params)
@@ -15,6 +19,17 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated!'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+  end
+
   private def article_params
     params.require(:article).permit(:title, :description)
   end
@@ -23,4 +38,5 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index; end
 end
