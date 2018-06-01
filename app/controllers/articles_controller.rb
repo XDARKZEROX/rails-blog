bb3b0a97f27058d918f65f21d8a9f79826fc9da9
Alexander Guzman <nocturnal.classical@hotmail.com>
2018-06-01 09:25:26 -0500

    Agregadas validaciones para articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,7 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     # @article = Article.new(article_params)
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = 'Article was succesfully created'
       redirect_to article_path(@article)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  has_many :articles
+  before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximun: 25 }
-  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, 
+    format: { with: VALID_EMAIL_REGEX }
 end
