b197b78f4cab69195c7c2d871eb3114a49dd26f0
Alexander Guzman <nocturnal.classical@hotmail.com>
2018-08-08 17:50:13 -0500

    added categories edit page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,11 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
-  def show;end
+  def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page:5)
+
+  end
 
   def create
     @category = Category.new(category_params)
@@ -19,6 +23,20 @@ class CategoriesController < ApplicationController
       redirect_to categories_path
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if(@category.update(category_params))
+      flash[:success] = 'Category was successfully updated'
+      redirect_to category_path(@category)
+    else
+      render 'edit'
     end
   end
 
