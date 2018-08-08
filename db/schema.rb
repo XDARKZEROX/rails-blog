3a41a837934fc205c2ace381b3cfdd59e22f5bdb
Alexander Guzman <nocturnal.classical@hotmail.com>
2018-08-08 16:43:14 -0500

    agregada correcion en el schema de la bd (no reconocia article_id

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 20180731215328) do
 
   create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
   end
 
   create_table "articles", force: :cascade do |t|
